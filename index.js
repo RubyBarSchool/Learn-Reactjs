36a92d4bd47c80380109ff3b3ca3cfa53006d210
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-05-22 22:40:03 +0700

    Chil_component

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+function HeaderComponent(){
+    return (
+        <header>
+            <nav>
+                Header
+            </nav>
+        </header>
+    )
+}
+
+function FooterComponent(){
+    return (
+        <footer>
+            <nav>
+                Footer
+            </nav>
+        </footer>
+    )
+}
+
 function MainContent(){
     return (
+        <div>
+        <HeaderComponent />
         <h1>I'm learning React!!</h1>
+        <FooterComponent />
+        </div>
     )
 }
 
