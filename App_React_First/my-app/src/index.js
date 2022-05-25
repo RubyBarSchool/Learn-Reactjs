12871c9c2ab9b9c9ce26a3b20d34e8b96fb47887
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-05-25 10:08:18 +0700

    update children component

--- App_React_First/my-app/src/index.js
+++ App_React_First/my-app/src/index.js
@@ -1,2 +1,26 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article>
+      <h1>{props.title}</h1>
+      <h4>{props.content}</h4>
+      <h4>{props.children}</h4>
+    </article>
+  );
+};
+
+function BookList() {
+  return (
+    <div>
+      <Book title="Title" content="content">
+        Children content
+      </Book>
+      <Book />
+    </div>
+  );
+}
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
---
+++ App_React_First/my-app/src/testing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const testing = () => {
+  return <div>testing</div>;
+};
+
+export default testing;
